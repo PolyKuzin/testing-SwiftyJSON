a67f77a1e98ed63765439e549d3e91738c6fe1ee
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-10-12 00:25:10 +0300

    parsingData with SwiftyJson to UITable View

--- testing-SwiftyJSON/TableViewController.swift
+++ testing-SwiftyJSON/TableViewController.swift
@@ -6,23 +6,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class TableViewController: UITableViewController {
-
+    
+    var titles : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        normalJSONCasting()
+    }
+    
+    //MARK: Normal data casting
+    func normalJSONCasting() {
+        AF.request("https://codewithchris.com/code/afsample.json", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                self.titles.append(json["firstkey"].stringValue)
+                for str in json["secondkey"].arrayValue {
+                    self.titles.append(str.stringValue)
+                }
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return titles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = titles[indexPath.row]
+        cell?.textLabel?.textColor = .black
+        
+        return cell!
     }
 }
